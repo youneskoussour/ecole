2986b930f62f8b9f6f9df9b485cb23aab68ed3de
pc <pc@DESKTOP-7JBLQPN>
2021-12-27 15:48:58 +0100

    Add project files.

---
+++ Ecole.Service.Model/BusinessLayer/Etudiant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecole.Service.Model.BusinessLayer
+{
+    public class Etudiant
+    {
+    }
+}
